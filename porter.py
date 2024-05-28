90f84f6e518a36677df3b1500682de2c3f2eb123
Lukasz Haratym <lharatym@quantcast.com>
2024-05-27 18:54:48 -0700

    chore: updated to latest `arrow`
    Additionally:
    * reformatted to `black` standards

--- porter.py
+++ porter.py
@@ -1,39 +1,45 @@
 from __future__ import print_function
-import sys
+
 import os
-import subprocess
-import arrow
 import shutil
+import subprocess
+import sys
+
+import arrow
 
 
 def fetch_creation_date(path):
     date_positon = 3
-    raw_output = subprocess.check_output(['exiftool', '-EXIF:CreateDate', path])
+    raw_output = subprocess.check_output(["exiftool", "-EXIF:CreateDate", path])
     raw_date = raw_output.split()[date_positon]
+    return arrow.get(raw_date.decode("utf-8"), "YYYY:MM:DD").date()
 
-    return arrow.get(raw_date, 'YYYY:MM:DD').date()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     source, destination = sys.argv[1], sys.argv[2]
 
     for dirpath, _, filenames in os.walk(source):
         for filename in filenames:
-            if filename[-3:] not in {'PEF', 'pef'}:
+            if filename[-3:] not in {"PEF", "pef"}:
+                continue
+
+            if filename[0] == ".":
                 continue
 
             source_photo_path = os.path.join(dirpath, filename)
             creation_date = fetch_creation_date(source_photo_path)
-            destination_photo_path = os.path.join(destination,
-                                                  str(creation_date.year),
-                                                  str(creation_date.month),
-                                                  str(creation_date.day),
-                                                  filename)
+            destination_photo_path = os.path.join(
+                destination,
+                str(creation_date.year),
+                str(creation_date.month).zfill(2),
+                str(creation_date.day).zfill(2),
+                filename,
+            )
             try:
                 os.makedirs(os.path.dirname(destination_photo_path))
             except OSError:
                 pass
 
-            print('Copying {} to {}'.format(source_photo_path, destination_photo_path))
+            print("Copying {} to {}".format(source_photo_path, destination_photo_path))
 
             shutil.copy(source_photo_path, destination_photo_path)
